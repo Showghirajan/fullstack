c0b4700bea5d2a2e65ff57018677b72b28278039
showghi <showghirajan9789@gmail.com>
2024-07-17 14:14:52 +0530

    final change

--- env/demo/demo/settings.py
+++ env/demo/demo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7sa#_%nszu*xp!fwr$)$5-ec5t0*ji%a^izk@@j%=gxx3$-rp4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
